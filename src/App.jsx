6f928077c0347bbbf84933ced2673573774b1642
BPLOdev <bplodev@gmail.com>
2024-08-19 11:03:16 +0800

    window title

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import Header from './header/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = 'LandTax'; // This will set the browser window title
+  }, []); // Empty dependency array ensures this runs only once when the component mounts
 
   return (
     <>
@@ -15,9 +19,9 @@ function App() {
       </div>
 
       <div className="card">
-      <h1>LAND TAX MANAGEMENT SYSTEM</h1>
+        <h1>LAND TAX MANAGEMENT SYSTEM</h1>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisdddicing elit. Nobis architecto repellendus quisquam repudiandae excepturi ullam magni quo consequatur aut. Excepturi iste ab sapiente molestiae recusandae error commodi officia at doloribus?
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis architecto repellendus quisquam repudiandae excepturi ullam magni quo consequatur aut. Excepturi iste ab sapiente molestiae recusandae error commodi officia at doloribus?
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum animi velit dolorem cum optio! Laboriosam alias ab in quisquam quam fuga optio nulla. Ipsam deleniti laborum dolorem officiis in architecto!
         </p>
       </div>
@@ -25,9 +29,7 @@ function App() {
         Click HERE to Start
       </p>
     </>
-  )
+  );
 }
 
-
-export default App
-
+export default App;
