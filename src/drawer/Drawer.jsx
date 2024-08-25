22af59436e34d36a4d8d298203e299dac884ecb7
BPLOdev <bplodev@gmail.com>
2024-08-25 19:46:03 +0800

    Drawer

--- src/drawer/Drawer.jsx
+++ src/drawer/Drawer.jsx
@@ -18,7 +18,7 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
-import logoo from '../assets/image/favicon.svg';
+import logo from '../assets/image/favicon.svg';
 import backgroundImage from '../assets/image/header-bg.svg';
 
 
@@ -129,6 +129,12 @@ export default function MiniDrawer() {
           >
             <MenuIcon />
           </IconButton>
+          <Typography sx={{
+            display: 'flex',
+            marginTop: '4px',
+            marginBottom: '4px', }}>
+          <img src={logo} alt="Logo" className="logo-img" />
+          </Typography>
           <Typography variant="h5" noWrap component="div">
             REAL PROPERTY TAX MANAGEMENT SYSTEM
           </Typography>
@@ -137,7 +143,7 @@ export default function MiniDrawer() {
 
 
       <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
+        <DrawerHeader sx={{height: '76px'}}>
           <IconButton onClick={handleDrawerClose}>
             {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
           </IconButton>
@@ -173,6 +179,19 @@ export default function MiniDrawer() {
         <DrawerHeader />
         <Typography paragraph>
             <h1>REAL PROPERTY TAX MANAGEMENT SYSTEM</h1>
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
         </Typography>
         <Typography paragraph>
           Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
