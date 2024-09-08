40d7a78fd348ec69ea340c746f17cf6e189a3b5d
BPLOdev <bplodev@gmail.com>
2024-09-08 10:03:09 +0800

    last update

--- src/AssessmentRoll/AssessmentRoll.jsx
+++ src/AssessmentRoll/AssessmentRoll.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import './AssessmentRoll.scss';
-import Header from '../header/Header';
 import MiniDrawer from '../drawer/Drawer';
 import Typography from '@mui/material/Typography';
 
--- src/drawer/Drawer.jsx
+++ src/drawer/Drawer.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
+import './Drawer.scss';
 import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar from '@mui/material/AppBar';
@@ -10,14 +11,10 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-// import InboxIcon from '@mui/icons-material/MoveToInbox';
-// import MailIcon from '@mui/icons-material/Mail';
 import logo from '../assets/image/favicon.svg';
 import backgroundImage from '../assets/image/header-bg.svg';
 
@@ -157,11 +154,8 @@ export default function MiniDrawer() {
       </AppBar>
 
       {/* onMouseEnter={handleDrawerOpen} onMouseLeave={handleDrawerClose} */}
-      <Drawer variant="permanent" open={open} >
+      <Drawer variant="permanent" open={open} onMouseLeave={handleDrawerClose}>
         <DrawerHeader sx={{height: '76px'}}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
         </DrawerHeader>
         <Divider />
         <List>
